740e49bd9771eb086eb7729a15900c4fc75fe3ed
Emily <fernandacq05@gmail.com>
2025-01-25 18:55:44 -0600

    I added my mail

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,6 +9,7 @@ function Footer() {
           <CardContact title="Instagram" text="@emily__0507" link="https://www.instagram.com/emily___0507/" />
           <CardContact title="Github" text="@EmitaQuiel" link="https://github.com/EmitaQuiel" />
           <CardContact title="Linkedin" text="Emily Castillo" link="https://www.linkedin.com/in/emi-castillo/" />
+          <CardContact title="Email" text="fernandacq05@gmail.com" link="mailto:fernandacq05@gmail.com" />
         </div>
       </div>
     </footer>
